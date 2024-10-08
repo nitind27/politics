e5b5f42aed1c65f4ef592d8a3f958acd270a636e
Nitin Dube <nitindube227@gmail.com>
2024-10-08 09:56:01 +0530

    fix bug

--- src/app/api/addmembers/route.ts
+++ src/app/api/addmembers/route.ts
@@ -1,7 +1,6 @@
+import prisma from '@/lib/db';
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
   const { name, contact, address } = await request.json();
