380d4e1b4403401fc2ead893a30b1354517d2fe1
Nitin Dube <nitindube227@gmail.com>
2024-10-08 09:49:28 +0530

    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import LoginForm from "@/components/auth/SignIn/signIn";
 import React from "react";
 
-const page = () => {
+import SignIn from "@/components/auth/SignIn/signIn";
+import SignInImg from "./(auth)/Signinimg/Signinimg";
+
+const Login = () => {
   return (
-    <div>
-      <div className="container mt-10 bg-white"><LoginForm /></div>
+    <div className="row d-flex vh-100 w-100 ">
+      <div
+        style={{ backgroundColor: "#F5F8FA" }}
+        className="col-lg-8 col-md-7 p-0 col-12  "
+      >
+        <SignInImg />
+      </div>
+      <div className="col-lg-4 col-md-5 col-12 p-0 bg-white">
+        <SignIn />
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Login;
